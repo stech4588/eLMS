02ce5258feed1b0c1c0665ecf014dfd5ed94c634
syeddaniyal <syeddaniyal6511@gmail.com>
2025-06-04 00:38:16 +0500

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role_id' => 1, // Assuming role_id 1 is for 'Super Admin'
         ]);
     }
 }
