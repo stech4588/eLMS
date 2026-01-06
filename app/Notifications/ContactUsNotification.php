f3dfb5d3c9ce44a0bafe3e719c7ca814221a0093
musmandev297 <mohammadusmandev297@gmail.com>
2026-01-06 15:47:43 +0500

    error checking

--- app/Notifications/ContactUsNotification.php
+++ app/Notifications/ContactUsNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ContactUsNotification extends Notification implements ShouldQueue
+class ContactUsNotification extends Notification
 {
     use Queueable;
 
